3b945b54e6c25407bca14823c97d8521ca39c6ab
Hack Reactor Student <student@hackreactor.com>
2017-03-30 19:50:37 -0700

    Hash tables complete

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,30 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //this._storage.set(index, v);
+  var objInsert = this._storage.get(index);
+  if(objInsert !== undefined){
+  	objInsert[k] = v;
+  } else {
+    objInsert = {};
+  	objInsert[k] = v;
+  }
+  this._storage.set(index, objInsert);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var objRetrieve = this._storage.get(index);
+  return objRetrieve[k];
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var objRemove = this._storage.get(index);
+  delete objRemove[k];
+  this._storage.set(index, objRemove);
+
 };
 
 
